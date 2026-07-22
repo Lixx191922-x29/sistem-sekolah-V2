52255701910b04b5db59022700839ee4c9f36c00
Lixx191922-x29 <felixfel0884@gmail.com>
2026-07-22 19:18:41 +0700

    feat: finish StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,10 +4,78 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+class TeacherController extends Controller
+{
+    public function index()
+    {
+        return "Ini adalah halaman daftar guru";
+    }
+
+    public function show($id)
+    {
+        return "Menampilkan detail guru dengan ID: {$id}";
+    }
+
+    public function create()
+    {
+        return "Ini adalah halaman untuk membuat guru baru";
+    }
+
+    public function store(Request $request)
+    {
+        return "Menyimpan data guru baru";
+    }
+
+    public function edit($id)
+    {
+        return "Ini adalah halaman untuk mengedit guru dengan ID: {$id}";
+    }
+
+    public function update(Request $request, $id)
+    {
+        return "Memperbarui data guru dengan ID: {$id}";
+    }
+
+    public function destroy($id)
+    {
+        return "Menghapus data siswa dengan ID: {$id}";
+    }
+}
+
 class StudentController extends Controller
 {
     public function index()
     {
         return "Ini adalah halaman daftar siswa";
+    }
+
+    public function show($id)
+    {
+        return "Menampilkan detail siswa dengan ID: {$id}";
+    }
+
+    public function create()
+    {
+        return "Ini adalah halaman untuk membuat siswa baru";
+    }
+
+    public function store(Request $request)
+    {
+        return "Menyimpan data siswa baru";
+    }
+
+    public function edit($id)
+    {
+        return "Ini adalah halaman untuk mengedit siswa dengan ID: {$id}";
+    }
+
+    public function update(Request $request, $id)
+    {
+        return "Memperbarui data siswa dengan ID: {$id}";
+    }
+
+    public function destroy($id)
+    {
+        return "Menghapus data siswa dengan ID: {$id}";
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\StudentsController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\Student\Controller;
 use App\Http\Controllers\MajorClass\CreateController;
-use App\Http\Controllers\MajorClass\destroyController;
-use App\Http\Controllers\MajorClass\editController;
-use App\Http\Controllers\MajorClass\indexController;
-use App\Http\Controllers\MajorClass\showController;
-use App\Http\Controllers\MajorClass\storeController;
-use App\Http\Controllers\MajorClass\updateController;
+use App\Http\Controllers\MajorClass\DestroyController;
+use App\Http\Controllers\MajorClass\EditController;
+use App\Http\Controllers\MajorClass\IndexController;
+use App\Http\Controllers\MajorClass\ShowController;
+use App\Http\Controllers\MajorClass\StoreController;
+use App\Http\Controllers\MajorClass\UpdateController;
 use App\Http\Controllers\MajorController; 
 
 use Illuminate\Support\Facades\Route;
@@ -18,22 +18,58 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Manajemen Data Siswa (Action)
-Route::name('student.')->prefix('students')->group(function (){
-    //Halaman Daftar Siswa
+// Manajemen Guru
+Route::name('teachers.')->prefix('teachers')->group(function () {
+    Route::get('/', [TeacherController::class, 'index'])->name('index');
+    
+    Route::get('/{id}', [TeacherController::class, 'show'])->name('show');
+
+    Route::get('/create', [TeacherController::class, 'create'])->name('create');
+
+    Route::post('/', [TeacherController::class, 'store'])->name('store');
+
+    Route::get('/{id}/edit', [TeacherController::class, 'edit'])->name('edit');
+
+    Route::put('/{id}', [TeacherController::class, 'update'])->name('update');
+
+    Route::delete('/{id}', [TeacherController::class, 'destroy'])->name('destroy');
+});
+
+// Manajemen Siswa
+Route::name('students.')->prefix('students')->group(function () {
     Route::get('/', [StudentController::class, 'index'])->name('index');
 
-    //Detail Siswa
-    Route::get('/{$id}', function(string $id){
-        return "Menampilkan siswa dengan ID: {$id}";
-    })->name('show');
+    Route::get('/{id}', [StudentController::class, 'show'])->name('show');
 
-    //Tambah Siswa
-    Route::get('/create', function(){
-        return "Menampilkan halaman tambah siswa";
-    })->name('create');
+    Route::get('/create', [StudentController::class, 'create'])->name('create');
 
-    //
-})
+    Route::post('/', [StudentController::class, 'store'])->name('store');
+
+    Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('edit');
+
+    Route::put('/{id}', [StudentController::class, 'update'])->name('update');
+
+    Route::delete('/{id}', [StudentController::class, 'destroy'])->name('destroy');
+});
+
+// Manajemen Kelas (Invokable)
+Route::name('classes.')->prefix('classes')->group(function () {
+    Route::get('/', IndexController::class)->name('index');
+
+    Route::get('/{id}', ShowController::class)->name('show');
+
+    Route::get('/create', CreateController::class)->name('create');
+
+    Route::post('/', StoreController::class)->name('store');
+
+    Route::get('/{id}/edit', EditController::class)->name('edit');
+
+    Route::put('/{id}', UpdateController::class)->name('update');
+
+    Route::delete('/{id}', DestroyController::class)->name('destroy');
+});
+
+// Manajemen Jurusan
+Route::resource('majors', MajorController::class);
 
 
