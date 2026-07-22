e0ae1b43bbcada892d44d274e253ebd6798970b5
Lixx191922-x29 <felixfel0884@gmail.com>
2026-07-22 13:53:24 +0700

    feat: wip Data Siswa

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,39 @@
 <?php
 
 use App\Http\Controllers\StudentsController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\Student\Controller;
+use App\Http\Controllers\MajorClass\CreateController;
+use App\Http\Controllers\MajorClass\destroyController;
+use App\Http\Controllers\MajorClass\editController;
+use App\Http\Controllers\MajorClass\indexController;
+use App\Http\Controllers\MajorClass\showController;
+use App\Http\Controllers\MajorClass\storeController;
+use App\Http\Controllers\MajorClass\updateController;
+use App\Http\Controllers\MajorController; 
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Manajemen Siswa
-route::name('students.')->prefix('students')->group(function() {
-        Route::get('/', [StudentsController::class, 'index'])->name('index');
+//Manajemen Data Siswa (Action)
+Route::name('student.')->prefix('students')->group(function (){
+    //Halaman Daftar Siswa
+    Route::get('/', [StudentController::class, 'index'])->name('index');
 
-Route::get('/{id}', function($id){
-    return "Menampilkan detail siswa dengan ID = {$id}";
-})->name('show');
+    //Detail Siswa
+    Route::get('/{$id}', function(string $id){
+        return "Menampilkan siswa dengan ID: {$id}";
+    })->name('show');
 
-});
+    //Tambah Siswa
+    Route::get('/create', function(){
+        return "Menampilkan halaman tambah siswa";
+    })->name('create');
+
+    //
+})
+
 
